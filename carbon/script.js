067b31580f7368c46be36b9fbb63ad9c76d49503
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-02-26 10:54:25 +0000

    added banner and share-banner codes

--- carbon/script.js
+++ carbon/script.js
@@ -1,3 +1,12 @@
-console.log("hi there");
-console.log("bye");
-//checking github commit 3
+console.log("vervel deployed");
+
+document
+  .querySelector('[data-banner-element="close"]')
+  .addEventListener("click", () => {
+    const banner = document.querySelector('[data-banner-element="banner"]');
+    const currentState = banner.getAttribute("data-banner-state");
+    banner.setAttribute(
+      "data-banner-state",
+      currentState === "visible" ? "hidden" : "visible",
+    );
+  });
