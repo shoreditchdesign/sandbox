5ca4260d9cf34cb29f81ecbf67b4bbe9efb35cb6
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-03-17 23:01:04 +0000

    Update ix.js

--- rivr/ix.js
+++ rivr/ix.js
@@ -1,65 +1,71 @@
-$(".swiper-main-component").each(function (index) {
-  let loopMode = false;
-  if ($(this).attr("loop-mode") === "true") {
-    loopMode = true;
-  }
-  let sliderDuration = 300;
-  if ($(this).attr("slider-duration") !== undefined) {
-    sliderDuration = +$(this).attr("slider-duration");
-  }
-  const swiper = new Swiper($(this).find(".swiper")[0], {
-    speed: sliderDuration,
-    autoHeight: false,
-    centeredSlides: loopMode,
-    followFinger: true,
-    freeMode: false,
-    slideToClickedSlide: false,
-    slidesPerView: 1,
-    spaceBetween: "4%",
-    rewind: false,
-    mousewheel: {
-      forceToAxis: true,
-    },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-    },
-    breakpoints: {
-      // mobile landscape
-      480: {
-        slidesPerView: 1,
-        spaceBetween: "4%",
+document.addEventListener("DOMContentLoaded", function () {
+  // Changed from jQuery selector to document.querySelectorAll for vanilla JS
+  document.querySelectorAll("#swiper-component").forEach(function (component) {
+    let loopMode = false;
+    if (component.getAttribute("loop-mode") === "true") {
+      loopMode = true;
+    }
+
+    let sliderDuration = 300;
+    if (component.getAttribute("slider-duration") !== undefined) {
+      sliderDuration = +component.getAttribute("slider-duration");
+    }
+
+    // Changed selector to find swiper element within the component
+    const swiperElement = component.querySelector("#swiper");
+
+    const swiper = new Swiper(swiperElement, {
+      speed: sliderDuration,
+      autoHeight: false,
+      centeredSlides: loopMode,
+      followFinger: true,
+      freeMode: false,
+      slideToClickedSlide: false,
+      slidesPerView: 1,
+      spaceBetween: "4%",
+      rewind: false,
+      loop: loopMode, // Added explicit loop setting based on attribute
+      mousewheel: {
+        forceToAxis: true,
       },
-      // tablet
-      768: {
-        slidesPerView: 2,
-        spaceBetween: "4%",
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
       },
-      // desktop
-      992: {
-        slidesPerView: 3,
-        spaceBetween: "2%",
+      breakpoints: {
+        // mobile landscape
+        480: {
+          slidesPerView: 1,
+          spaceBetween: "4%",
+        },
+        // tablet
+        768: {
+          slidesPerView: 2,
+          spaceBetween: "4%",
+        },
+        // desktop
+        992: {
+          slidesPerView: 3,
+          spaceBetween: "2%",
+        },
       },
-    },
-    pagination: {
-      el: $(this).find(".swiper-bullet-wrapper")[0],
-      bulletActiveClass: "is-active",
-      bulletClass: "swiper-bullet",
-      bulletElement: "button",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: $(this).find(".swiper-next")[0],
-      prevEl: $(this).find(".swiper-prev")[0],
-      disabledClass: "is-disabled",
-    },
-    scrollbar: {
-      el: $(this).find(".swiper-drag-wrapper")[0],
-      draggable: true,
-      dragClass: "swiper-drag",
-      snapOnRelease: true,
-    },
-    slideActiveClass: "is-active",
-    slideDuplicateActiveClass: "is-active",
+      // Removed pagination config since there's no .swiper-bullet-wrapper in your HTML
+
+      navigation: {
+        // Changed to use direct ID selectors matching your HTML
+        nextEl: "#swiper-next",
+        prevEl: "#swiper-prev",
+        disabledClass: "is-disabled",
+      },
+      scrollbar: {
+        // Changed to use direct ID selector matching your HTML
+        el: "#swiper-drag-wrapper",
+        draggable: true,
+        dragClass: "swiper-drag",
+        snapOnRelease: true,
+      },
+      slideActiveClass: "is-active",
+      slideDuplicateActiveClass: "is-active",
+    });
   });
 });
