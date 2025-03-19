f970f79e84b1a0c10f865c2f191821f04d98cf8c
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-03-19 17:14:50 +0000

    Update script.js

--- rivr/script.js
+++ rivr/script.js
@@ -3,6 +3,41 @@ window.intercomSettings = {
   app_id: "jey3sjgt",
 };
 
+// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/jey3sjgt'
+(function () {
+  var w = window;
+  var ic = w.Intercom;
+  if (typeof ic === "function") {
+    ic("reattach_activator");
+    ic("update", w.intercomSettings);
+  } else {
+    var d = document;
+    var i = function () {
+      i.c(arguments);
+    };
+    i.q = [];
+    i.c = function (args) {
+      i.q.push(args);
+    };
+    w.Intercom = i;
+    var l = function () {
+      var s = d.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.intercom.io/widget/jey3sjgt";
+      var x = d.getElementsByTagName("script")[0];
+      x.parentNode.insertBefore(s, x);
+    };
+    if (document.readyState === "complete") {
+      l();
+    } else if (w.attachEvent) {
+      w.attachEvent("onload", l);
+    } else {
+      w.addEventListener("load", l, false);
+    }
+  }
+})();
+
 document
   .querySelector('[data-nav-element="menu"]')
   .addEventListener("click", () => {
