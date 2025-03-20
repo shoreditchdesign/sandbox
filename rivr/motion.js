9047fb6c76757560b488b2c1c5c1024322b93c40
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-03-20 14:22:27 +0000

    Update motion.js

--- rivr/motion.js
+++ rivr/motion.js
@@ -22,7 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //GSAP for Headings
-//GSAP for Headings
 window.addEventListener("DOMContentLoaded", (event) => {
   setTimeout(() => {
     if (typeof gsap === "undefined" || typeof SplitType === "undefined") {
@@ -390,6 +389,7 @@ document.addEventListener("DOMContentLoaded", function () {
           trigger: container,
           start: "top 70%", // Start when the top of the container reaches 75% of viewport
           markers: false, // Set to true for debugging
+          once: true, // Add once: true to prevent replaying the animation
           onEnter: () => {
             console.log(
               `Container ${containerIndex + 1} entered viewport, playing animation`,
