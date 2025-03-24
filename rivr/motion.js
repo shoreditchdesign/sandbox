384d268e1da7fcb911c2dcf5ae578335f03bc552
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-03-24 10:35:14 +0000

    Update motion.js
    Co-Authored-By: Austin Joseph <124405650+austinjxseph@users.noreply.github.com>

--- rivr/motion.js
+++ rivr/motion.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (isDesktop()) {
     // Select navbars that don't have the blocked attribute
     const navbars = document.querySelectorAll(
-      '[data-nav-element="navbar"]:not([data-nav-block="blocked"])',
+      '[data-nav-element="navbar"]:not([data-slide-block="blocked"])',
     );
 
     if (navbars.length === 0) return;
