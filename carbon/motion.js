7adface8fa704eee7778613aa3e3b8d1df27f7cd
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-04-29 14:45:48 +0100

    Update motion.js
    Co-Authored-By: Austin Joseph <124405650+austinjxseph@users.noreply.github.com>

--- carbon/motion.js
+++ carbon/motion.js
@@ -129,7 +129,7 @@ document.addEventListener("DOMContentLoaded", () => {
         initialOpacity: 0,
         finalOpacity: 1,
         ease: "power2.out",
-        initialDelay: 0.6,
+        initialDelay: 0,
         staggerDelay: 0.3,
       },
       text: {
