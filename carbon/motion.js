6f97a0a971693a4796610c022e1fb53ddde18455
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-05-19 12:29:29 +0100

    feat: delay to remove flicks

--- carbon/motion.js
+++ carbon/motion.js
@@ -1175,7 +1175,7 @@ window.addEventListener("DOMContentLoaded", () => {
           markers: false,
           once: true,
           onEnter: () => {
-            tl.play(0);
+            tl.play(0.2);
           },
         });
       }
@@ -1263,7 +1263,7 @@ document.addEventListener("DOMContentLoaded", function () {
             markers: false,
             once: true,
             onEnter: () => {
-              tl.play(0);
+              tl.play(0.2);
             },
           });
         }
@@ -1346,7 +1346,7 @@ document.addEventListener("DOMContentLoaded", function () {
           markers: false,
           once: true,
           onEnter: () => {
-            tl.play(0); // No delay here!
+            tl.play(0.2);
           },
         });
       }
