5618b5e0625af0b9d5730976620d71ebddeb7790
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-05-22 00:50:26 +0100

    Update motion.js

--- carbon/motion.js
+++ carbon/motion.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
         initialPosition: "100%",
         finalPosition: "0%",
         ease: "power2.out",
-        initialDelay: 3.1,
+        initialDelay: 2.6,
         staggerDelay: 0.3,
         fadeInDuration: 0.1,
       },
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", () => {
         initialOpacity: 0,
         finalOpacity: 1,
         ease: "power2.out",
-        initialDelay: 3.7,
+        initialDelay: 3.2,
         staggerDelay: 0.3,
       },
       text: {
