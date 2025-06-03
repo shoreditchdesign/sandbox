802efab97560acf49dfb3120bd9ebd91f504684e
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-06-03 14:01:50 +0100

    Update ix.js
    Co-Authored-By: Austin Joseph <124405650+austinjxseph@users.noreply.github.com>

--- carbon/ix.js
+++ carbon/ix.js
@@ -16,16 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //Navigation Banner
 document.addEventListener("DOMContentLoaded", function () {
-  document
-    .querySelector('[data-banner-element="close"]')
-    .addEventListener("click", () => {
-      const banner = document.querySelector('[data-banner-element="banner"]');
-      const currentState = banner.getAttribute("data-banner-state");
-      banner.setAttribute(
-        "data-banner-state",
-        currentState === "visible" ? "hidden" : "visible",
-      );
-    });
+  const closeButton = document.querySelector('[data-banner-element="close"]');
+  if (!closeButton) return;
+
+  closeButton.addEventListener("click", () => {
+    const banner = document.querySelector('[data-banner-element="banner"]');
+    const currentState = banner.getAttribute("data-banner-state");
+    banner.setAttribute(
+      "data-banner-state",
+      currentState === "visible" ? "hidden" : "visible",
+    );
+  });
 });
 
 //Navbar Border
