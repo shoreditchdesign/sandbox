9aee2a18b9ddcd50f67c50b2a16ad59eae933ef5
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-06-18 11:34:54 +0100

    Update motion.js

--- carbon/motion.js
+++ carbon/motion.js
@@ -1678,7 +1678,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       gsap.set(element, {
         opacity: 1,
-        top: "-100%",
+        translateY: "-100%",
       });
 
       const tl = gsap.timeline({
@@ -1687,7 +1687,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       tl.to(element, {
         opacity: 1,
-        top: "0%",
+        translateY: "0%",
         duration: 0.8,
         ease: "power2.out",
       });
