5e1d41b28004f5b11eabca30cc31009baa1c8bbc
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-07-17 12:15:25 +0100

    Update script.js

---
+++ mexem/script.js
@@ -0,0 +1,11 @@
+//Textarea Resizer
+document.addEventListener("DOMContentLoaded", () => {
+  const textareas = document.querySelectorAll("textarea");
+
+  textareas.forEach((textarea) => {
+    textarea.addEventListener("input", () => {
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    });
+  });
+});
