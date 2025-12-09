518dd4714231ae0e848801a66940c272b06b205b
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-12-09 14:59:06 +0000

    drag fix

--- engine/ix.js
+++ engine/ix.js
@@ -1183,8 +1183,8 @@ document.addEventListener("DOMContentLoaded", function () {
     slidesPerView: 1,
     slidesPerGroup: 1,
     spaceBetween: 16,
-    grabCursor: false,
-    allowTouchMove: false,
+    grabCursor: true,
+    allowTouchMove: true,
     autoHeight: false,
     watchOverflow: true,
     slidesOffsetBefore: 0,
@@ -1219,10 +1219,6 @@ document.addEventListener("DOMContentLoaded", function () {
     watchSlidesProgress: false,
     watchSlidesVisibility: false,
     breakpoints: {
-      0: {
-        allowTouchMove: true,
-        grabCursor: true,
-      },
       992: {
         slidesPerView: 2,
         spaceBetween: 16,
